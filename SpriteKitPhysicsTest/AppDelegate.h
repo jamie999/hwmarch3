532669e894eacc133df5c8827bd5a6454e43df9c
JamieHong <jamiehongbydesign@gmail.com>
2014-03-03 19:33:31 -0500

    Initial Commit

---
+++ SpriteKitPhysicsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKitPhysicsTest
+//
+//  Created by Jamie Hong on 3/3/14.
+//  Copyright (c) 2014 JamieHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
